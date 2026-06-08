9f62f9b9d5f0964fc90b4729345e4e04eaafaff4
abdurrahmanshaikh1 <abdurrahmanshaikh156@gmail.com>
2026-06-08 21:18:09 +0530

    add about route

--- server/src/app.js
+++ server/src/app.js
@@ -1,19 +1,27 @@
 const express = require("express");
 const path = require("path");
-const app= express();
+const app = express();
 
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 // contribution from sarang branch
-app.get("/", (req,res)=>{
+app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
-})
+});
 
-app.get("/health",(req,res)=>{
+app.get("/health", (req, res) => {
   res.json({
-    message:"healthy",
+    message: "healthy",
     success: true
-  })
-})
+  });
+});
+
+// contribution from abdur branch
+app.get("/about", (req, res) => {
+  res.json({
+    message: "about route added by abdur",
+    success: true
+  });
+});
 
 module.exports = app;
