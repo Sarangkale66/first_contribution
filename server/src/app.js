a9069d313cdec722f22481dfffbf6662b8ff31db
asynchronousRohit <rohit.pokhariya123@gmail.com>
2026-06-08 22:44:54 +0530

    pushing in rohit branch

--- server/src/app.js
+++ server/src/app.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, "..", "public")));
 app.get("/", (req,res)=>{
   res.sendFile(path.join(__dirname, "..", "public", "index.html"));
 })
-
+//Rohit Test 2
 app.get("/health",(req,res)=>{
   res.json({
     message:"healthy",
